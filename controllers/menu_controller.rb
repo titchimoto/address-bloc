d6a22aabd30564efe733fac9c1b0d58449684466
Titch Deane <titch@hotmail.co.uk>
2018-01-30 12:11:53 -0800

    Checkpoint Menus Continued: Complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -90,9 +90,85 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+  end
+
+  def search_submenu
+    puts "\nd - Delete Entry"
+    puts "e - Edit this Entry"
+    puts "m - Return to Main Menu"
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted."
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print = "Updated Phone Number: "
+    phone_number = gets.chomp
+    print "Updated Email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry."
+    puts entry
   end
 
   def entry_submenu(entry)
@@ -107,8 +183,11 @@ class MenuController
       when "n"
 
       when "d"
+        delete_entry(entry)
 
       when "e"
+        edit_entry(entry)
+        entry_submenu(entry)
 
       when "m"
         system "clear"
